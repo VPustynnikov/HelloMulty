652cba94ced56ed4b0c4c0f2b16d471a3a925595
Pustynnikov-VV <pustynnkov@mail.ru>
2016-10-21 16:02:56 +0700

    21.10.2016 multymodule project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: SBT-Pustynnikov-VV
+ * Date: 21.10.16
+ * Time: 14:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main (String[] args) {
+    System.out.println("Hello World"); }
+}
